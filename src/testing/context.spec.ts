c012189ea42039c8181b42d21ed78cb186455014
Flo <florent.vouin@gmail.com>
2023-05-15 11:59:57 +0200

    ✨ Implement the createContext testing utility

---
+++ src/testing/context.spec.ts
@@ -0,0 +1,63 @@
+import { pino } from 'pino';
+import { ConfigurationReader } from '../configuration/reader.js';
+import { BaseConfiguration, WorkspaceFunction } from '../index.js';
+import { createContext } from './context.js';
+
+abstract class MyDefinition extends WorkspaceFunction<string> {}
+
+class MyImplementation extends MyDefinition {
+  _call(): string {
+    return '🎉';
+  }
+
+  _supports(): boolean {
+    return true;
+  }
+}
+
+describe('context', () => {
+  describe('createContext', () => {
+    it('should create a context with options', () => {
+      const expectedWorkingDirectory = '/some/work/dir';
+      const expectedRootPath = '/some/root/path';
+      const expectedProjectPath = '/some/project/path';
+      const expectedLogger = pino();
+
+      const actualTestContext = createContext({
+        workingDirectory: expectedWorkingDirectory,
+        rootPath: expectedRootPath,
+        projectPath: expectedProjectPath,
+        configuration: { workspace: { name: '💮' }, someOther: { value: 123 } },
+        logger: expectedLogger,
+        functions: [MyImplementation],
+      });
+
+      const actualContext = actualTestContext.context;
+      expect(actualContext.logger).toBe(expectedLogger);
+      expect(actualContext.workingDirectory).toEqual(expectedWorkingDirectory);
+      expect(actualContext.rootPath).toEqual(expectedRootPath);
+      expect(actualContext.projectPath).toEqual(expectedProjectPath);
+      expect(actualContext.call(MyDefinition, {})).toEqual('🎉');
+      expect(actualTestContext.configuration).toBe(
+        (actualContext as any).configuration,
+      );
+      expect(actualTestContext.functionRegistry).toBe(
+        (actualContext as any).functionRegistry,
+      );
+      expect(actualContext.get('someOther.value')).toEqual(123);
+    });
+
+    it('should use the passed ConfigurationReader', () => {
+      const expectedConfigurationReader =
+        new ConfigurationReader<BaseConfiguration>([]);
+
+      const actualTestContext = createContext({
+        configuration: expectedConfigurationReader,
+      });
+
+      expect((actualTestContext.context as any).configuration).toBe(
+        expectedConfigurationReader,
+      );
+    });
+  });
+});
---
+++ src/testing/context.ts
@@ -0,0 +1,132 @@
+import { resolve } from 'path';
+import { Logger, pino } from 'pino';
+import {
+  ConfigurationReader,
+  ConfigurationReaderSourceType,
+} from '../configuration/index.js';
+import {
+  BaseConfiguration,
+  ProcessorInstruction,
+  WorkspaceContext,
+  WorkspaceFunction,
+} from '../context/index.js';
+import {
+  FunctionRegistry,
+  ImplementableFunctionImplementationConstructor,
+} from '../function-registry/index.js';
+
+/**
+ * A {@link WorkspaceContext}, along with its private components, useful for tests.
+ */
+export type TestWorkspaceContext = {
+  /**
+   * The {@link WorkspaceContext}.
+   */
+  context: WorkspaceContext;
+
+  /**
+   * The {@link ConfigurationReader} used by the context.
+   */
+  configuration: ConfigurationReader<BaseConfiguration>;
+
+  /**
+   * The {@link FunctionRegistry} used by the context.
+   */
+  functionRegistry: FunctionRegistry<WorkspaceContext>;
+};
+
+/**
+ * Creates a {@link WorkspaceContext} without loading the configuration nor the modules.
+ * A configuration object (or a {@link ConfigurationReader}) can be passed instead, and functions can be registered
+ * either using the `functions` options, or the returned {@link FunctionRegistry}.
+ *
+ * @param options Options when creating the {@link WorkspaceContext}.
+ * @returns The {@link WorkspaceContext}.
+ */
+export function createContext(
+  options: {
+    /**
+     * The working directory for the context. Defaults to the process current directory.
+     * This is **not** used to load the configuration.
+     */
+    workingDirectory?: string;
+
+    /**
+     * The environment for the context.
+     * This is **not** used to update the configuration.
+     */
+    environment?: string | null;
+
+    /**
+     * The root path for the context. Defaults to the `workingDirectory`.
+     * This is **not** inferred from the configuration.
+     */
+    rootPath?: string;
+
+    /**
+     * The project path for the context. Defaults to the `workingDirectory`.
+     * This is **not** inferred from the configuration.
+     */
+    projectPath?: string | null;
+
+    /**
+     * The configuration for the context. Defaults to a configuration where only `workspace.name` is set to `test`.
+     * If a plain object is passed, it is loaded as a (fake) configuration file.
+     */
+    configuration?:
+      | ConfigurationReader<BaseConfiguration>
+      | (Partial<BaseConfiguration> & Record<string, any>);
+
+    /**
+     * The logger for the context.
+     */
+    logger?: Logger;
+
+    /**
+     * A list of function implementations that will be registered.
+     */
+    functions?: ImplementableFunctionImplementationConstructor<
+      WorkspaceFunction<any>
+    >[];
+  } = {},
+): TestWorkspaceContext {
+  const workingDirectory = resolve(options.workingDirectory ?? process.cwd());
+  const environment =
+    options.environment !== undefined ? options.environment : null;
+  const rootPath = resolve(options.rootPath ?? workingDirectory);
+  const projectPath =
+    options.projectPath !== undefined ? options.projectPath : workingDirectory;
+
+  const configuration =
+    options.configuration instanceof ConfigurationReader
+      ? options.configuration
+      : new ConfigurationReader<BaseConfiguration>([
+          {
+            configuration: options.configuration ?? {
+              workspace: { name: 'test' },
+            },
+            source: 'causa.yaml',
+            sourceType: ConfigurationReaderSourceType.File,
+          },
+        ]);
+
+  const functionRegistry = new FunctionRegistry(WorkspaceFunction);
+  functionRegistry.registerImplementations(...(options.functions ?? []));
+
+  const processors: ProcessorInstruction[] = [];
+
+  const logger = options.logger ?? pino();
+
+  const context = new (WorkspaceContext as any)(
+    workingDirectory,
+    environment,
+    rootPath,
+    projectPath,
+    configuration,
+    functionRegistry,
+    processors,
+    logger,
+  );
+
+  return { context, configuration, functionRegistry };
+}
