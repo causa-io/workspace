a16b7eadfb63287fe4eee564abaf22c0407a9edc
Flo <florent.vouin@gmail.com>
2023-05-19 07:52:49 +0200

    💥 Define the ProcessorFunction interface and get the configuration as a property in the output

--- src/context/context.processor.module.test.ts
+++ src/context/context.processor.module.test.ts
@@ -1,13 +1,25 @@
 import { IsString } from 'class-validator';
 import { WorkspaceFunction } from './functions.js';
 import { ModuleRegistrationFunction } from './modules.js';
+import { ProcessorFunction, ProcessorOutput } from './processor.js';
 
-export class MyProcessor extends WorkspaceFunction<any> {
+export class MyProcessor
+  extends WorkspaceFunction<
+    Promise<{
+      configuration: Record<string, any>;
+      someOtherOutput: string;
+    }>
+  >
+  implements ProcessorFunction
+{
   @IsString()
   readonly value!: string;
 
-  async _call(): Promise<any> {
-    return { myProcessorConf: this.value };
+  async _call() {
+    return {
+      configuration: { myProcessorConf: this.value },
+      someOtherOutput: this.value,
+    };
   }
 
   _supports(): boolean {
@@ -15,12 +27,15 @@ export class MyProcessor extends WorkspaceFunction<any> {
   }
 }
 
-export class MyOtherProcessor extends WorkspaceFunction<any> {
+export class MyOtherProcessor
+  extends WorkspaceFunction<Promise<ProcessorOutput>>
+  implements ProcessorFunction
+{
   @IsString()
   readonly value!: string;
 
-  async _call(): Promise<any> {
-    return { myOtherProcessorConf: this.value };
+  async _call() {
+    return { configuration: { myOtherProcessorConf: this.value } };
   }
 
   _supports(): boolean {
---
+++ src/context/processor.ts
@@ -0,0 +1,34 @@
+import { WorkspaceFunction } from './functions.js';
+
+/**
+ * Describes a {@link ProcessorFunction} to run when initializing the workspace context.
+ * The return value of the function will be used to update the configuration.
+ */
+export type ProcessorInstruction = {
+  /**
+   * The name of the {@link WorkspaceFunction}.
+   */
+  name: string;
+
+  /**
+   * Arguments for the workspace function.
+   */
+  args?: Record<string, any>;
+};
+
+/**
+ * The return value of a {@link ProcessorFunction}.
+ */
+export type ProcessorOutput = {
+  /**
+   * A partial configuration that will be merged into the workspace configuration.
+   */
+  configuration: Record<string, any>;
+};
+
+/**
+ * A {@link WorkspaceFunction} that can be used as a processor when creating a workspace context.
+ * Its return value should contain a configuration object that will be merged into the workspace configuration.
+ */
+export interface ProcessorFunction
+  extends WorkspaceFunction<Promise<ProcessorOutput>> {}
