62e5a06fb75443f177a0a589d2e1d55660326a69
Flo <florent.vouin@gmail.com>
2026-02-09 09:45:39 +0100

    ✨ Export the path to the base configuration JSONSchema file

--- src/context/base-configuration.ts
+++ src/context/base-configuration.ts
@@ -1,4 +1,14 @@
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
 import type { BaseConfigurationSchema, Workspace } from './generated.js';
+
+/**
+ * The path to the JSON Schema file defining the base configuration.
+ */
+export const BASE_CONFIGURATION_SCHEMA_PATH = resolve(
+  fileURLToPath(import.meta.url),
+  './schemas/base-configuration.yaml',
+);
 
 /**
  * The schema for core elements of the configuration.
